9830f9adc2b3202a23018ba7fd99bb184a4f002d
Catalina Williams <catalina.rose1361@gmail.com>
2019-11-14 15:32:54 -0700

    html draft rendered, js starting points added

---
+++ assets/jQuery/main.js
@@ -0,0 +1,20 @@
+// // {/* <html>
+// <head>
+// <title></title>
+// <script>
+// function myFunction() { 
+// 	document.getElementById("mainFrameOne").style.display="none"; 
+// 	document.getElementById("mainFrameTwo").style.display="block"; 
+// }
+// </script>
+// </head>
+// <body>
+// <div id="mainFrameOne">
+//     <p>mainFrameOne</p>
+// </div>
+// <div id="mainFrameTwo" style="display:none;">
+//     <p>mainFrameTwo</p>
+// </div>
+// <button onclick="myFunction()">Click me</button>
+// </body>
+// </html> */}
