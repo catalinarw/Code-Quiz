fedbf090ce2023722d065a1035e97ba9f09a2de0
Catalina Williams <catalina.rose1361@gmail.com>
2019-11-17 16:24:19 -0700

    functional timer that appears on screen

--- assets/jQuery/main.js
+++ assets/jQuery/main.js
@@ -1,24 +1,42 @@
 var score = 0;
+var secondsLeft = 75;
 var timer = document.querySelector("#timer")
 var startButton = document.querySelector("#startButton");
 var startDiv = document.querySelector("#firstDiv");
 var QnADiv = document.querySelector("#secondDiv");
 var allDoneDiv = document.querySelector("#thirdDiv");
 var highScoresDiv = document.querySelector("#fourthDiv");
+startDiv.style.visibility = "visible";
 timer.style.visibility = "hidden";
+QnADiv.style.visibility = "hidden";
+allDoneDiv.style.visibility = "hidden";
+highScoresDiv.style.visibility = "hidden";
 
-// 1. user clicks start quiz button
-startButton.addEventListener("click", startTimer);
-function startTimer() {
-timer.style.visibility = "visible";
+// startButton.addEventListener("click", startTimer);
 
-}
-
-   
-
-
-// 2. timer appears
+// function startTimer() {
+    // timer.style.visibility = "visible";
+    
+    // }
+    // 1. user clicks start quiz button
+startButton.addEventListener("click", function startQuiz() {
+    // 2. timer appears
+    timer.style.visibility = "visible";
+    startDiv.style.visibility = "hidden";
+    QnADiv.style.visibility = "visible";
+    
+    });
 // 3. timer starts
+setInterval(function() {
+    secondsLeft--;
+    if (secondsLeft === 0) {
+        QnADiv.style.visibility = "hidden";
+        allDoneDiv.style.visibility = "visible";
+        timer.style.visibility = "hidden";
+    } else {
+        timer.textContent = "time:" + secondsLeft;
+    }
+  }, 1000)
 // 4. 1st div display none
 // 5. 2nd div display visible
 
