da835bad2e45eea5094ffeaf7a669ad5ff6bf278
Catalina Williams <catalina.rose1361@gmail.com>
2019-11-16 20:10:56 -0700

    starting JS, timer to show on-click

--- assets/jQuery/main.js
+++ assets/jQuery/main.js
@@ -1,20 +1,78 @@
-// // {/* <html>
-// <head>
-// <title></title>
-// <script>
-// function myFunction() { 
-// 	document.getElementById("mainFrameOne").style.display="none"; 
-// 	document.getElementById("mainFrameTwo").style.display="block"; 
-// }
-// </script>
-// </head>
-// <body>
-// <div id="mainFrameOne">
-//     <p>mainFrameOne</p>
-// </div>
-// <div id="mainFrameTwo" style="display:none;">
-//     <p>mainFrameTwo</p>
-// </div>
-// <button onclick="myFunction()">Click me</button>
-// </body>
-// </html> */}
+var score = 0;
+var timer = document.querySelector("#timer")
+var startButton = document.querySelector("#startButton");
+var startDiv = document.querySelector("#firstDiv");
+var QnADiv = document.querySelector("#secondDiv");
+var allDoneDiv = document.querySelector("#thirdDiv");
+var highScoresDiv = document.querySelector("#fourthDiv");
+timer.style.visibility = "hidden";
+
+// 1. user clicks start quiz button
+startButton.addEventListener("click", startTimer);
+function startTimer() {
+timer.style.visibility = "visible";
+
+}
+
+   
+
+
+// 2. timer appears
+// 3. timer starts
+// 4. 1st div display none
+// 5. 2nd div display visible
+
+// 1. user clicks answer
+    // if correct, 
+        // "correct" appears on screen
+        // add one to score 
+    // if not correct 
+        // "incorrect" appears on screen
+        // time decreases
+// 2. new question and answers populate 
+// 3. repeat for each question
+    //  once last question is answered timer stops
+// 4. 2nd div display none
+// 5. 3rd div display visible 
+
+// 1. display score 
+// 2. when submitt button clicked, 
+    // add user input to highscores list 
+    // save to local storage 
+    // 3rd div display none
+    // 4th div display visible
+    // view highscore button display none
+
+// 1. display high score list 
+// 2. if go back is clicked
+    // 4th div display none
+    // 1st div display visible
+// 3. if clear high scores is clicked
+    // list is cleared and removed from local storage
+
+// if timer runs out, brings to div 3
+
+
+
+
+// 1st div display none, second div display visible
+// possible timer function
+// function changeValue() {
+//     document.getElementById("demo").innerHTML = ++value;
+//   }
+  
+//   var timerInterval = null;
+//   function start() {
+//     stop(); // stoping the previous counting (if any)
+//     value = 0;
+//     timerInterval = setInterval(changeValue, 1000);  
+//   }
+//   var stop = function() {
+//     clearInterval(timerInterval);
+//   }
+//   <p>A script on this page starts this clock:</p>
+  
+//   <p id="demo">0</p>
+  
+//   <button onclick="start()">Start time</button>
+//   <button onclick="stop()">Stop time</button>
